9284964d87665d558980d531372f920145e81e67
Todd Wolfson <todd@twolfson.com>
2012-12-03 10:31:51 -0800

    More tweaking to get files to properly open

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 ln -s $PWD/.bashrc ~/.bashrc
 ln -s $PWD/.bash_profile ~/.bash_profile
-cat .desktop >> ~/.local/share/applications/.desktop
+ln -s $PWD/sublime-text-2.desktop ~/.local/share/applications/sublime-text-2.desktop
 
 # cat /usr/share/applications/defaults.list | sed 's/gedit.desktop/sublime-text-2.desktop;gedit.desktop/'
 ln -s $PWD/defaults.list ~/.local/share/applications/defaults.list
