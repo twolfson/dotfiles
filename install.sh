ee9b54d4e07a3320d3b6b3656ad7d772427a6944
Todd Wolfson <todd@twolfson.com>
2012-12-30 15:18:31 -0800

    Adding in more configurations

--- install.sh
+++ install.sh
@@ -4,3 +4,9 @@ ln -s $PWD/sublime-text-2.desktop ~/.local/share/applications/sublime-text-2.des
 
 # cat /usr/share/applications/defaults.list | sed 's/gedit.desktop/sublime-text-2.desktop;gedit.desktop/'
 ln -s $PWD/defaults.list ~/.local/share/applications/defaults.list
+
+# Fonts
+mkdir -p ~/.fonts
+
+# I no longer use powerline (traded it in for Sexy Bash Prompt) but still like Anonymous Pro
+ln -s $PWD/.fonts/anonymous+Pro-Powerline.ttf ~/.fonts/anonymous+Pro-Powerline.ttf
