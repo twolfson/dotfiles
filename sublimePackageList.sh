89f7727f318a77ec0dab5b4772ca727d01568a86
Todd Wolfson <todd@twolfson.com>
2013-01-21 02:18:11 -0800

    Dealing with grep and regexp

--- sublimePackageList.sh
+++ sublimePackageList.sh
@@ -1,4 +1,8 @@
 PACKAGES=$(ls -1 ~/".config/sublime-text-2/Packages")
 DEFAULT_PACKAGES=$(ls -1 ~/".config/sublime-text-2/Pristine Packages" | sed -e "s/\.sublime-package$//")
-#echo $PACKAGES | grep --invert-match $DEFAULT_PACKAGES
-echo $(echo $DEFAULT_PACKAGES | sed -e "s/./|/g")
+DEFAULT_REGEXP=$(echo $DEFAULT_PACKAGES | tr " " "|")
+DEFAULT_REGEXP="/("$DEFAULT_REGEXP")/"
+# echo $DEFAULT_REGEXP
+# echo $PACKAGES | grep --invert-match -e $DEFAULT_REGEXP
+echo $PACKAGES | tr " " "\n" | grep -e "LESS"
+echo $PACKAGES | tr " " "\n" | grep -e "LESS|YAML"
