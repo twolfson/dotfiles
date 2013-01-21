08dc518f975ee20313d504d4f2d307a11342b8a3
Todd Wolfson <todd@twolfson.com>
2013-01-21 02:24:57 -0800

    More hashing out of sublimePackageList

--- sublimePackageList.sh
+++ sublimePackageList.sh
@@ -1,8 +1,9 @@
-PACKAGES=$(ls -1 ~/".config/sublime-text-2/Packages")
-DEFAULT_PACKAGES=$(ls -1 ~/".config/sublime-text-2/Pristine Packages" | sed -e "s/\.sublime-package$//")
-DEFAULT_REGEXP=$(echo $DEFAULT_PACKAGES | tr " " "|")
-DEFAULT_REGEXP="/("$DEFAULT_REGEXP")/"
+PACKAGES=$(ls -1 ~/".config/sublime-text-2/Packages" | tr "\n" "!")
+# DEFAULT_PACKAGES=$(ls -1 ~/".config/sublime-text-2/Pristine Packages" | sed -e "s/\.sublime-package$//")
+DEFAULT_REGEXP=$(ls -1 ~/".config/sublime-text-2/Pristine Packages" | sed -e "s/\.sublime-package$//" | tr "\n" "|")
+DEFAULT_REGEXP=$(echo $DEFAULT_REGEXP | sed -e "s/|$//")
 # echo $DEFAULT_REGEXP
-# echo $PACKAGES | grep --invert-match -e $DEFAULT_REGEXP
-echo $PACKAGES | tr " " "\n" | grep -e "LESS"
-echo $PACKAGES | tr " " "\n" | grep -e "LESS|YAML"
+echo $DEFAULT_REGEXP
+echo $PACKAGES
+# echo $PACKAGES | tr "!" "\n" | grep --invert-match -E $DEFAULT_REGEXP
+# echo $PACKAGES | tr " " "\n" | grep -E 'LESS|YAML'
