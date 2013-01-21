5264d48ef1932964b4d4a5cde14e0d21c4ba97d5
Todd Wolfson <todd@twolfson.com>
2013-01-21 02:31:04 -0800

    Generated sublimePackageList

--- sublimePackageList.sh
+++ sublimePackageList.sh
@@ -1,10 +1,5 @@
 PACKAGES=$(ls -1 ~/".config/sublime-text-2/Packages" | tr "\n" "!")
-# DEFAULT_PACKAGES=$(ls -1 ~/".config/sublime-text-2/Pristine Packages" | sed -e "s/\.sublime-package$//")
 DEFAULT_REGEXP=$(ls -1 ~/".config/sublime-text-2/Pristine Packages" | sed -e "s/\.sublime-package$//" | tr "\n" "|")
 DEFAULT_REGEXP=$(echo $DEFAULT_REGEXP | sed -e "s/|$//")
-# echo $DEFAULT_REGEXP
-# echo $DEFAULT_REGEXP
-# echo $PACKAGES
-echo $PACKAGES | tr "!" "\n" | grep --invert-match -E "$DEFAULT_REGEXP"
-# echo $PACKAGES | tr "!" "\n" | grep --invert-match -E 'Default|CSS|C#|C++|Default|Diff|D|Erlang|Go|Graphviz|Groovy|Haskell|HTML|JavaScript|Java'
-# echo $PACKAGES | tr " " "\n" | grep -E 'LESS|YAML'
+INSTALLED_PACKAGES=$(echo $PACKAGES | tr "!" "\n" | grep --invert-match -E "$DEFAULT_REGEXP" | tr "\n" "!")
+echo $INSTALLED_PACKAGES | tr "!" "\n" > ./sublimePackageList.txt
