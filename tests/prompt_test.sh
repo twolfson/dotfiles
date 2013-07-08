8fce1f8f06fffbea01ff3c2e035f68149bd8baac
Todd Wolfson <todd@twolfson.com>
2013-07-07 19:27:29 -0700

    Finished writing out test cases

--- tests/prompt_test.sh
+++ tests/prompt_test.sh
@@ -22,6 +22,9 @@
 
     # is 'no branch'
 
+  # TODO: Should we ever list tag?
+  # TODO: What do you think about master~1 as a case?
+
 # git_status
 
   # on a clean and synced branch
@@ -32,12 +35,28 @@
 
     # is an asterisk
 
-  # on an unsynced branch
+  # on an unpushed branch
+  # DEV: This covers new branches
 
-    # is an empty triangle
+    # is an empty up triangle
 
-  # on a dirty and unsynced branch
+  # on a dirty and unpushed branch
 
-    # is a filled triangle
+    # is a filled up triangle
 
   # TODO: These ones...
+  # on an unpulled branch
+
+    # is an empty down triangle
+
+  # on a dirty and unpulled branch
+
+    # is an filled down triangle
+
+  # on an unpushed and an unpulled branch
+
+    # is an empty hexagon
+
+  # on a dirty, unpushed, and unpulled branch
+
+    # is an filled hexagon
