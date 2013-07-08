6d95f2f16c6baee8b19f3e011e0eb7072605f64d
Todd Wolfson <todd@twolfson.com>
2013-07-07 22:57:28 -0700

    Finished up 'it works enough' demo script

--- demo.sh
+++ demo.sh
@@ -2,25 +2,33 @@
 # Walks to the next directory in our list
 
 # Save the TEST_DIR for reference
-TEST_DIR=test/test-files/
+TEST_DIR="test/test-files"
+
+# If we are already in a test directory, use ../
+if test -n "$(echo $PWD | grep 'test-files')"; then
+  TEST_DIR=".."
+fi
 
 # Navigate through the test directories
 USE_NEXT_DIR=""
-for DEMO_DIR in "" "clean-synced" "dirty" "unsynced" "dirty-unsynced" ""; do
+TARGET_DEMO_DIR=""
+for DEMO_DIR in "clean-synced" "dirty" "unpushed" "dirty-unpushed"; do
   # If we should use this directory, navigate to it
   if test -n "$USE_NEXT_DIR"; then
-    echo "Use $DEMO_DIR"
+    TARGET_DEMO_DIR="$DEMO_DIR"
     break
   fi
 
   # If the current directory was the last one, mark us to use the next one
-  if test "$DEMO_DIR" = "$_LAST_DEMO_DIR"; then
+  if test -n "$(echo $PWD | grep -E ${DEMO_DIR}$)"; then
     USE_NEXT_DIR="1"
   fi
 done
 
-# If no directories were matching, reset _LAST_DEMO_DIR
-if test -z "$USE_NEXT_DIR"; then
-  echo "We couldn't find where you were in the loop. Resetting it..."
-  _LAST_DEMO_DIR=""
+# If no directories were matching, move to the start directory
+if test -z "$TARGET_DEMO_DIR"; then
+  TARGET_DEMO_DIR="clean-synced"
 fi
+
+# Move to the directory
+echo "$TEST_DIR"/"$TARGET_DEMO_DIR"
