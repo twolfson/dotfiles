b16e874e423bc8d3abe288866413b84284368784
Todd Wolfson <todd@twolfson.com>
2013-07-07 19:04:43 -0700

    Catching errors. Time to go off to the races =D

--- test.sh
+++ test.sh
@@ -1,2 +1,10 @@
 # TODO: Move this into a Makefile
-bash --rcfile .nah --noprofile --norc -i -c "./tests/prompt_test.sh" 2> test.stderr
+bash --rcfile .nah --noprofile -i -c "./tests/prompt_test.sh" 2> test.stderr
+
+# If there were test failures, fail
+STDERR=$(cat test.stderr)
+if test -n "$STDERR"; then
+  echo "ERRORS OCCURRED. STDERR OUTPUT:" 1>&2
+  echo $STDERR 1>&2
+  exit 1
+fi
