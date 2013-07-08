a773d3f64bf79a0bf82b8cd449e81ef83f61fd5c
Todd Wolfson <todd@twolfson.com>
2013-07-07 21:47:08 -0700

    Tried to do fixture/unfixture for tests but it failed. Moving back

--- tests/prompt_test.sh
+++ tests/prompt_test.sh
@@ -2,9 +2,9 @@
 _PWD=$PWD
 TEST_DIR=$PWD/tests
 
-# Fixture all git directories
+# Unfixture all git directories (all .git to dotgit)
 echo "Setting up..."
-"$TEST_DIR"/utils/fixture_git.sh
+"$TEST_DIR"/utils/unfixture_git.sh
 
 # TODO: Write a demo.sh which moves through the test dirs
 # TODO This would be practical for demo.sh, a pre-commit hook, and a pre-test command
@@ -13,6 +13,7 @@ fixture_dir() {
   TMP_DIR=$(mktemp -d)
   cp -r "$TEST_DIR"/test-files/$1/* $TMP_DIR
   cd $TMP_DIR
+  test -d dotgit && mv dotgit .git
 }
 
 # Load in bashrc
@@ -22,12 +23,14 @@ fixture_dir() {
 
   # in a git directory
   fixture_dir 'git'
+  ls -la
 
     # returns a non-empty string
     test "$(is_on_git)" != "" || echo '`is_on_git` === `false` in git directory' 1>&2
 
   # in a non-git directory
   fixture_dir 'non-git'
+  ls -la
 
     # returns an empty string
     test "$(is_on_git)" = "" || echo '`is_on_git` === `true` in non-git directory' 1>&2
@@ -84,10 +87,3 @@ fixture_dir() {
   # on a dirty, unpushed, and unpulled branch
 
     # is an filled hexagon
-
-# Unfixture all git directores
-cd $_PWD
-echo ""
-echo "Cleaning up..."
-"$TEST_DIR"/utils/unfixture_git.sh
-echo ""
