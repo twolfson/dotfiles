f708772ebfa08931b22b906ebc3101da7afcd3fb
Todd Wolfson <todd@twolfson.com>
2013-09-28 16:15:27 -0700

    Beginning on python pre/post release scripts

--- git-template-dir/hooks/post-release.sh
+++ git-template-dir/hooks/post-release.sh
@@ -5,3 +5,12 @@ if test -f package.json; then
   # If the package.json does not contain a 'private: true', publish it
   node -e "f = './package.json'; p = require(f); process.exit(+(p.private||0))" && npm publish
 fi
+
+if test -f setup.py; then
+  # If the package is at 0.1.0, register it
+  if grep "version='0.1.0'" setup.py; then
+    echo 'hi'
+  fi
+
+  # Build and upload the package
+fi
